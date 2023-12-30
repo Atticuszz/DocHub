e5d3a548876f9406370e206ae9cf8e9ce5358563
Atticus Zhou <1831768457@qq.com>
2023-12-30 15:59:52 +0800

    update script
    Signed-off-by: Atticus Zhou <1831768457@qq.com>

--- scripts/main.py
+++ scripts/main.py
@@ -7,7 +7,7 @@
 """
 from logs.config import setup_logging
 from readme_handler import update_readme
-from scripts.convert_links import convert_wiki_links_in_dir
+from convert_links import convert_wiki_links_in_dir
 
 if __name__ == '__main__':
     setup_logging()
