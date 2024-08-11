f9870095e4b0c48ade1dea0097e6cd63fbe56b74
Atticuszz <1831768457@qq.com>
2024-08-11 17:29:17 +0800

    fix citation error in caption and first columns fixed in one row

--- essay/texs/GsplatLoc/table_filter.py
+++ essay/texs/GsplatLoc/table_filter.py
@@ -5,35 +5,31 @@ This filter does the following:
 
 It's intended use is for scientific papers that require 2 columns in the template layotu.
 """
-
 import sys
-
 from panflute import *
+
+
+def get_text(item):
+    if isinstance(item, Str):
+        return item.text
+    elif isinstance(item, Cite):
+        return get_text(item.content)  # Raw content
+    elif isinstance(item, Plain):
+        return ''.join([get_text(i) for i in item.content])
+    elif isinstance(item, ListContainer):
+        return ''.join([get_text(i) for i in item])
+    else:
+        raise TypeError('Unsupported filter type ! ' + type(item))
 
 def replace_longtables_with_tabular(elem, doc):
     try:
-        def get_text(item):
-            if isinstance(item, Str):
-                return item.text
-            elif isinstance(item, Plain):
-                return ''.join([get_text(i) for i in item.content])
-            elif isinstance(item, ListContainer):
-                return ''.join([get_text(i) for i in item])
-            else:
-                return str(item)
-
-        def caption():
-            if elem.caption and elem.caption.content:
-                return '\\caption{' + get_text(elem.caption.content) + '}\n' + \
-                    '\\label{table:' + (elem.identifier or '') + '}\n'
-            return ''
-
         def tabular():
-            return '\\begin{tabular}{' + 'l' * elem.cols + '}\n\\hline\n'
+            # 使用 l 作为第一列，其余列使用 X,防止换因为名称出现'-'行
+            return '\\begin{tabularx}{\\textwidth}{l' + 'X' * (elem.cols - 1) + '}\n\\toprule\n'
 
         def headers():
             if elem.head and elem.head.content:
-                return ' & '.join([get_text(cell.content) for cell in elem.head.content[0].content]) + '\\\\\n\\hline\n'
+                return ' & '.join([get_text(cell.content) for cell in elem.head.content[0].content]) + '\\\\\n\\midrule\n'
             return ''
 
         def items():
@@ -43,12 +39,19 @@ def replace_longtables_with_tabular(elem, doc):
                     rows.append(' & '.join([get_text(cell.content) for cell in row.content]) + '\\\\')
             return '\n'.join(rows) + '\n'
 
-        result = '\\begin{table*}[t]\n\\centering\n' + \
-                 caption() + \
+        def caption():
+            if elem.caption and elem.caption.content:
+                return '\\caption{' + get_text(elem.caption.content) + '}\n' + \
+                    '\\label{table:' + (elem.identifier or '') + '}\n'
+            return ''
+
+        result = '\\begin{table*}[htbp]\n\\centering\n' + \
                  tabular() + \
                  headers() + \
                  items() + \
-                 '\\hline\n\\end{tabular}\n\\end{table*}'
+                 '\\bottomrule\n\\end{tabularx}\n' + \
+                 caption() + \
+                 '\\end{table*}'
 
         print("Table processed successfully", file=sys.stderr)
         return RawBlock(result, 'latex')
@@ -73,12 +76,11 @@ def action(elem, doc):
 def finalize(doc):
     pass
 
-# keep this structure: see http://scorreia.com/software/panflute/guide.html
 def main(doc=None):
     return run_filter(action,
-                         prepare=prepare,
-                         finalize=finalize,
-                         doc=doc)
+                      prepare=prepare,
+                      finalize=finalize,
+                      doc=doc)
 
 
 if __name__ == '__main__':
